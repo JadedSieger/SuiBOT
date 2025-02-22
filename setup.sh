aea2638c1a87d68b59ec7e5a6cc4e46b8d6cdc95
Simpcast Productions <66287897+JadedSieger@users.noreply.github.com>
2025-02-22 23:57:22 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo "Updating package list and dependencies"
-sudo apt-get update && sudo apt-get install -y ffmpeg
+echo "Updating package list and installing dependencies"
+apt-get update && apt-get install -y ffmpeg
 
 echo "Installing yt-dlp"
-sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
-sudo chmod a+rx /usr/local/bin/yt-dlp
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+chmod a+rx /usr/local/bin/yt-dlp
 
+echo "Verifying yt-dlp installation"
+yt-dlp --version || echo "yt-dlp installation failed!"
 
 echo "Installation Complete"
